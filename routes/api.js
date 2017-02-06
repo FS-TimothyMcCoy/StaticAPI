4223949c52159933279e7d266ce33c5df667960d
FS-TimothyMcCoy <tjfatterstacks@live.com>
2017-02-06 16:50:15 -0500

    Add api js and get routes set up

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,14 @@
-module.export = function (express) {
+module.exports = function (express) {
 var router = express.Router();
+
+router.get('/', function(req, res){
+res.json({hello: "world"});
+});
+
+router.get('/status', function(req, res){
+res.json({healthy: "true"});
+
+});
 
   return router;
 }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
-var app = express();
-
 var body_parser = require('body-parser');
+
+var app = express();
 
 var port = 3000;
 
-app.use('/api', require('./routes/api.js')(express));
+app.use('/api', require('../routes/api.js')(express));
 
 app.listen(port, function() {
   console.log('Server Active on', port);
